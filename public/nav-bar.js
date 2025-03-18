b66baacd546002e25eb72cc0e0fa6e5cb212ff87
Jay-Lee Shih <rayjebb@gmail.com>
2025-03-18 10:20:41 +0200

    removing app search

--- public/nav-bar.js
+++ public/nav-bar.js
@@ -1,7 +1,6 @@
 const nav = document.querySelector('nav');
 const pages = [
     { name: "Home", href: "index.html"},
-    { name: "App Search", href: "app-search.html"},
     { name: "Package Search", href: "package-search.html"}
 ]
 
